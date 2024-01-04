b380985e0c71bfee17ef9940e2abd93bb6eaa644
Luca van Straaten <lucavanstraaten@icloud.com>
2024-01-04 22:47:54 +0100

    update pin assignments

--- src/PinAssignments.h
+++ src/PinAssignments.h
@@ -1,24 +1,19 @@
-// PinAssignments.h
 #ifndef PIN_ASSIGNMENTS_H
 #define PIN_ASSIGNMENTS_H
 
 // Display
-#define DISPLAY_CHIP_SELECT_PIN 21      // CS
-#define DISPLAY_DATA_COMMAND_PIN 17     // DC
-#define DISPLAY_RESET_PIN 16            // RES
-//                           MOSI 23
+#define DISPLAY_CHIP_SELECT_PIN 21
+#define DISPLAY_DATA_COMMAND_PIN 17
+#define DISPLAY_RESET_PIN 16
 
 // Pulse input
 #define PULSE_INPUT_PIN 13
 
 // CAN bus
-#define MCP2515_CS_PIN 23
-#define MCP2515_INT_PIN 22
+#define MCP2515_CS_PIN 32
+#define MCP2515_INT_PIN 15
 
-// #define MOSI_PIN 23
-// #define MISO_PIN 19
-// #define SCK_PIN 18
-
+// Gauge
 #define GaugeRX 27
 #define GaugeTX 12
 
