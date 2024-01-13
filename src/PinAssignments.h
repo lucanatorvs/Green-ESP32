a0f0aac7711ddb7305fd167fdd98cd8fdc7ec8de
Luca van Straaten <lucavanstraaten@icloud.com>
2024-01-13 11:23:58 +0100

    pulse input to non led pin

--- src/PinAssignments.h
+++ src/PinAssignments.h
@@ -7,11 +7,11 @@
 #define DISPLAY_RESET_PIN 16
 
 // Pulse input
-#define PULSE_INPUT_PIN 13
+#define PULSE_INPUT_PIN 4
 
 // CAN bus
 #define MCP2515_CS_PIN 32
-#define MCP2515_INT_PIN 15
+#define MCP2515_INT_PIN 15 
 
 // Gauge
 #define GaugeRX 27
