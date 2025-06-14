1dad603e1e219a6d6379bd3ee719e0d5e324b304
lucanatorvs <lucavanstraaten@icloud.com>
2025-06-14 17:48:39 +0200

    check and fix all pin assignments

--- src/PinAssignments.h
+++ src/PinAssignments.h
@@ -1,41 +1,41 @@
 #ifndef PIN_ASSIGNMENTS_H
 #define PIN_ASSIGNMENTS_H
 
-// Display
-#define DISPLAY_CHIP_SELECT_PIN 21      // IO21
-#define DISPLAY_DATA_COMMAND_PIN 17     // IO17 (TX)
-#define DISPLAY_RESET_PIN 16            // IO16 (RX)
-
-// Pulse input
-#define PULSE_INPUT_PIN 4               // A5
-
 // CAN bus
-#define CAN_TX_PIN 14                        // IO14
-#define CAN_RX_PIN 32                        // IO32
-
-// Gauge
-#define GaugeRX 27                      // IO27
-#define GaugeTX 12                      // IO12
-
-// Button
-#define BUTTONPIN 33                   // IO33
-
-// Buzzer
-#define BUZZER_PIN 14                   // IO14
-
-// door close sensor
-#define DOOR_CLOSE_SENSOR_PIN 39        // A3
-
-// ignition switch
-#define IGNITION_SWITCH_PIN 36          // A4
+#define CAN_TX_PIN 14
+#define CAN_RX_PIN 32
 
 // Brake light output
-#define BRAKE_LIGHT_PIN 15              // IO15
+#define BRAKE_LIGHT_PIN 15
+
+// Button
+#define BUTTONPIN 33
+
+// Gauge
+#define GaugeRX 27
+#define GaugeTX 12
+
+// Buzzer
+#define BUZZER_PIN 13
+
+// Display
+#define DISPLAY_CHIP_SELECT_PIN 21
+#define DISPLAY_DATA_COMMAND_PIN 17
+#define DISPLAY_RESET_PIN 16
 
 // Lamp output
-#define SOC_Lamp_PIN 34                 // A3
-#define TEMPERATURE_Lamp_PIN 4          // A5
-#define BATTERY_Lamp_PIN 23             // IO23 (SDA)
-#define RUNNIG_Lamp_PIN 22              // IO22 (SCL)
+#define BATTERY_Lamp_PIN 23
+#define RUNNIG_Lamp_PIN 22
+#define SOC_Lamp_PIN 25
+#define TEMPERATURE_Lamp_PIN 4
+
+// Pulse input
+#define PULSE_INPUT_PIN 26
+
+// door close sensor
+#define DOOR_CLOSE_SENSOR_PIN 39
+
+// ignition switch
+#define IGNITION_SWITCH_PIN 36
 
 #endif // PIN_ASSIGNMENTS_H
